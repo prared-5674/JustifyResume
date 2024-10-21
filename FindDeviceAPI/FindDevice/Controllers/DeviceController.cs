f9de76e03caa138aa59907841e4a9ecc9242edea
prathapa <prathapa@golighthouse.com>
2024-10-20 23:13:50 -0700

    API To save device and see device existance in database

---
+++ FindDeviceAPI/FindDevice/Controllers/DeviceController.cs
@@ -0,0 +1,32 @@
+﻿using FindDevice.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FindDevice.Controllers
+{
+    [ApiController]
+    [Route("api/device")]
+    public class DeviceController : ControllerBase
+    {
+        private static Dictionary<string, List<string>> tenantData = new Dictionary<string, List<string>>();
+
+        // POST: api/device/register
+        [HttpPost("register")]
+        public IActionResult RegisterDevice([FromBody] DeviceModel device, [FromHeader(Name = "tenantId")] string tenantId)
+        {
+            if (!tenantData.ContainsKey(tenantId))
+                tenantData[tenantId] = new List<string>();
+
+            tenantData[tenantId].Add(device.DeviceName);
+            return Ok("Device Registered Successfully!");
+        }
+
+        // GET: api/device/data/{tenantId}
+        [HttpGet("data/{tenantId}")]
+        public IActionResult GetTenantData(string tenantId)
+        {
+            if (tenantData.ContainsKey(tenantId))
+                return Ok(tenantData[tenantId]);
+            return NotFound("Tenant Not Found");
+        }
+    }
+}
---
+++ FindDeviceAPI/FindDevice/ViewModel/DeviceModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FindDevice.ViewModel
+{
+    public class DeviceModel
+    {
+        public string DeviceName { get; set; }
+        public int DeviceId { get; set; }
+    }   
+}
